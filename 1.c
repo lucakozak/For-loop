9908c3d1c4c73710162fa79da2ed8a9dd526330f
lucakozak <lucakozak@icloud.com>
2017-10-26 13:06:15 +0200

    1.

---
+++ 1.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  1
+//
+//  Created by Kozak, Luca on 2017. 09. 29..
+//  Copyright © 2017. Kozak, Luca. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    
+    int a;
+    for (a=1; a<=9; a++){
+        
+        printf("%d\n", a);
+    }
+    
+    
+    return 0;
+}
